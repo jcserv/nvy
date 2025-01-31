eecfbd097e8c8a544fc64e8ffe0f1311bdc2541e
jcserv <jarrod.servilla@gmail.com>
2025-01-30 20:12:28 -0500

    feat: error reporting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 
-// Search for a pattern in a file and display the lines that contain it.
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
     /// The pattern to look for
@@ -9,7 +10,17 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
+
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
